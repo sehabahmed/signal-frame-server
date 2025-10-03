6fd3f85e51ac5be5d1cdd79c2334abbb079c9897
Sehab Ahmed <sehabahmed50100@gmail.com>
2025-10-03 19:37:04 +0200

    feat: jwt web token middleware and verifyJWT utils created

--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -1,7 +1,21 @@
 import express from "express";
+import auth from "../../middlewares/auth";
+import { USER_ROLE } from "./user.constant";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { UserValidation } from "./user.validation";
+import { UserControllers } from "./user.controller";
 
 const router = express.Router();
 
 export const UserRoutes = router;
 
-router.post('/create-user', )
+router.post(
+  "/create-user",
+  auth(USER_ROLE.ADMIN),
+  validateRequest(UserValidation.createUserValidationSchema),
+  UserControllers.userRegister
+);
+
+router.get("/", UserControllers.getAllUsers);
+
+router.get("/:id", UserControllers.getSingleUser);
--- src/app/utils/verifyJWT.ts
+++ src/app/utils/verifyJWT.ts
@@ -1,1 +1,31 @@
-export const createToken = ()
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import AppError from "../errors/AppError";
+import { USER_ROLE, USER_STATUS } from "../modules/User/user.constant";
+import jwt, { JwtPayload, Secret, SignOptions } from "jsonwebtoken";
+
+type UserJWTPayload = {
+  _id?: string;
+  name: string;
+  email: string;
+  mobileNumber?: string;
+  role: keyof typeof USER_ROLE;
+  status: keyof typeof USER_STATUS;
+};
+
+export const createToken = (
+  jwtPayload: UserJWTPayload,
+  secret: Secret,
+  expiresIn: string
+): string => {
+  const options: SignOptions = { expiresIn:  expiresIn as unknown as SignOptions['expiresIn'] };
+  return jwt.sign(jwtPayload, secret, options);
+};
+
+export const verifyToken = (token: string, secret: string): JwtPayload | Error => {
+    try {
+        return jwt.verify(token, secret) as JwtPayload;
+    } catch (error: any) {
+        throw new AppError(401, "You are not authorized!");
+    }
+}
