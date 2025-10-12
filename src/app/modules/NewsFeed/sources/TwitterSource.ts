12a31a93b897fd74b5a13036de4b01f69366c874
sehabahmed <freelancersehab1@gmail.com>
2025-10-12 14:50:44 +0200

    update: Twitter source bearer token updated successfully.

--- src/app/modules/NewsFeed/sources/TwitterSource.ts
+++ src/app/modules/NewsFeed/sources/TwitterSource.ts
@@ -1,6 +1,7 @@
+import config from "../../../config";
 import { INewsSource } from "../newsfeed.interface";
 
 export class TwitterSource implements INewsSource {
-    private readonly bearerToken =
+    private readonly bearerToken = config
     private readonly url =
 }
