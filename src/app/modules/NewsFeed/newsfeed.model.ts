578f144f99e6fd5d47238e3df0206cb9f5b01974
sehabahmed <freelancersehab1@gmail.com>
2025-10-15 22:21:11 +0200

    update (newsfeed mode): add popularity and source metrics mongoose model schema

--- src/app/modules/NewsFeed/newsfeed.model.ts
+++ src/app/modules/NewsFeed/newsfeed.model.ts
@@ -40,6 +40,24 @@ const newsFeedSchema = new Schema<TNewsItem>(
       type: String,
       enum: ["positive", "neutral", "negative"],
     },
+
+    popularity: {
+      score: { type: Number, default: 0, index: true },
+      views: { type: Number, default: 0 },
+      clicks: { type: Number, default: 0 },
+      shares: { type: Number, default: 0 },
+      bookmarks: { type: Number, default: 0 },
+      comments: { type: Number, default: 0 },
+      upvotes: { type: Number, default: 0 },
+      lastCalculated: { type: Date, default: Date.now },
+    },
+    sourceMatrics: {
+        hackerNewsScore: Number,
+        hackerNewsComments: Number,
+        twitterLikes: Number,
+        twitterRetweets: Number,
+        twitterReplies: Number,
+    }
   },
   { timestamps: true }
 );
