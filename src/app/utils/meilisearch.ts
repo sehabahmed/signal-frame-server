496707391a739f881a70c4c4b6c09d454bb74ba8
sehabahmed <freelancersehab1@gmail.com>
2025-11-07 11:21:36 +0100

    update (meiliSearch utils): add addDocumentToIndex and deleteDocumentToIndex to the meiliSearch Utils

---
+++ src/app/utils/meilisearch.ts
@@ -0,0 +1,51 @@
+import { MeiliSearch } from "meilisearch";
+import config from "../config";
+import { TNewsItem } from "../modules/NewsFeed/newsfeed.interface";
+import { Types } from "mongoose";
+
+const meiliClient = new MeiliSearch({
+  host: config.meilisearch_host as string,
+  apiKey: config.meilisearch_master_key,
+});
+
+export async function addDocumentToIndex(
+  result: Document<unknown, object, TNewsItem> &
+    TNewsItem & { _id: Types.ObjectId },
+  indexKey: string
+) {
+  const index = meiliClient.index(indexKey);
+
+  const { _id, title, content, url, source, author, popularity } = result;
+
+  const document = {
+    id: _id.toString(),
+    title,
+    content,
+    url,
+    source,
+    author,
+    popularity,
+  };
+
+  try {
+    await index.addDocuments([document]);
+  } catch (error) {
+    console.error("Error adding document to MeiliSearch", error);
+  }
+}
+
+export const deleteDocumentFromIndex = async (indexKey: string, id: string) => {
+  const index = meiliClient.index(indexKey);
+
+  try {
+    await index.deleteDocument(id);
+  } catch (error) {
+    console.error("Error deleting resource from MeiliSearch:", error);
+  }
+};
+
+export const deleteMeiliSearchIndex = async (indexKey: string) => {
+    meiliClient.deleteIndex(indexKey);
+}
+
+export default meiliClient;
